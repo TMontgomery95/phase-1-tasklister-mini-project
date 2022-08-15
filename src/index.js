a3afae451906d87b0e7f0ec9b1fa811bd38d3a3c
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-15 13:37:03 -0400

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.taskDescription.value);
+    form.reset();
+  });
 });
+
+// Creates tasks and handles deletions
+function buildToDo(todo){
+  let li = document.createElement('li');
+  li.textContent = `${todo}`
+  document.querySelector('#tasks').appendChild(li);
+}
